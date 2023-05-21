7047032a5c6a9d89a4872bb0d8f435a3a1e9c6f7
jahirsheikh <jahirsheikh14@gmail.com>
2023-05-21 09:05:39 +0600

    add

---
+++ script.js
@@ -0,0 +1,23 @@
+// let x=100;
+// let y=500 ;
+// let result=x+y;
+// console.log(result);
+
+// let a=500;
+// let b=800;
+// let sum=b-a;
+// console.log(sum)
+
+
+
+let radius=4;
+const pi=3.1416;
+let areaOfCircle=pi*radius*radius;
+console.log(areaOfCircle)
+
+
+let radiuse=2;
+const pie=3.14165
+let areaOfCirclek=pie*radiuse*radiue;
+
+console.log(areaOfCirclek);
